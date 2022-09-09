4cb793f05c51516f583d746af6008334f9a25921
mbalabanov <marincomics@gmail.com>
2022-09-09 21:48:16 +0300

    Completes like functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const pokeForm = document.querySelector(".poke-form")
 const pokeList = document.querySelector(".poke-list")
-const likedPokemons = []
+let likedPokemons = []
 
-function addPokemon(pokemon) {
+function createPokemonCard(pokemon) {
   const liEl = document.createElement("li")
   const imgEl = document.createElement("img")
   const h2El = document.createElement("h2")
   const deleteButton = document.createElement("button")
   const likeButton = document.createElement("button")
+  const likeImage = document.createElement("img")
 
   liEl.classList.add("pokemon")
   imgEl.src = pokemon.image
@@ -18,25 +19,41 @@ function addPokemon(pokemon) {
   deleteButton.setAttribute('id', 'delete' + pokemon.id)
   deleteButton.setAttribute('class', 'deleteButton')
 
-
-
   likeButton.innerText = 'Like'
   likeButton.setAttribute('id', 'like' + pokemon.id)
   likeButton.setAttribute('class', 'likeButton')
 
-  liEl.append(imgEl, h2El, likeButton, deleteButton)
+  likeImage.setAttribute('src', 'assets/heart.svg')
+  likeImage.setAttribute('alt', pokemon.name)
+  likeImage.setAttribute('width', '10px')
+  likeImage.classList.add('notLiked')
+
+  if (findLikedPokemons(pokemon.id)) {
+    likeImage.classList.remove('notLiked')
+    likeImage.classList.add('liked')
+  }
+
+  liEl.append(likeImage, imgEl, h2El, likeButton, deleteButton)
   pokeList.append(liEl)
 
   deleteButton.addEventListener('click', function (e) {
     deletePokemon(pokemon.id)
   });
   likeButton.addEventListener('click', function (e) {
-    likePokemon(pokemon.id)
+    if (!findLikedPokemons(pokemon.id)) {
+      likePokemon(pokemon.id)
+      likeImage.classList.remove('notLiked')
+      likeImage.classList.add('liked')
+    } else {
+      likedPokemons = removeLikedPokemon(pokemon.id)
+      likeImage.classList.remove('liked')
+      likeImage.classList.add('notLiked')
+    }
   });
 }
 
 function addPokemons(pokemons) {
-  pokemons.forEach(pokemon => addPokemon(pokemon))
+  pokemons.forEach(pokemon => createPokemonCard(pokemon))
 }
 
 function listenToAddPokemonForm() {
@@ -66,24 +83,30 @@ function postNewPokemon(newPokemon) {
     body: JSON.stringify(newPokemon)
   })
     .then(res => res.json())
-    .then(pokemon => addPokemon(pokemon))
+    .then(pokemon => createPokemonCard(pokemon))
 }
 
 function deletePokemon(pokemonId) {
+  likedPokemons = removeLikedPokemon(pokemonId)
   fetch("http://localhost:3000/pokemons/" + pokemonId, {
     method: "DELETE"
   })
 }
 
 function likePokemon(pokemonId) {
-  if(!findLikedPokemons(pokemonId)) {
+  if (!findLikedPokemons(pokemonId)) {
     likedPokemons.push(pokemonId)
   }
-  console.log(likedPokemons)
+}
+
+function removeLikedPokemon(pokemonId) { 
+  return likedPokemons.filter(function(pokemonItem){ 
+      return pokemonItem != pokemonId; 
+  });
 }
 
 function findLikedPokemons(pokemonId) {
-  if(likedPokemons.find(element => element === pokemonId)) {
+  if (likedPokemons.find(element => element === pokemonId)) {
     return true
   }
   return false
