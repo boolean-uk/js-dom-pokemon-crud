acd96ada40084a9a09fdd58bf6483d153fb30235
Tim Zoltie <t.zoltie@gmail.com>
2024-04-05 12:08:45 +0100

    deleteBtn_working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = 'https://boolean-api-server.fly.dev/tzoltie/pokemon'
 const pokemonForm = document.querySelector('#poke-form')
 const nameInput = document.querySelector('#name-input')
-const urlSearch = document.querySelector('#image-input')
+const urlInput = document.querySelector('#image-input')
 const cardList = document.querySelector('.cards')
 
 
@@ -15,11 +15,19 @@ function getCards() {
 }
 
 
-
 function createCard(name, image) {
     const listItem = document.createElement('li')
     listItem.classList.add('card')
     
+    const deleteBtn = document.createElement('input')
+    deleteBtn.classList.add('delete-button')
+    deleteBtn.type = 'button'
+    deleteBtn.value = 'X'
+    listItem.append(deleteBtn)
+
+    deleteBtn.addEventListener('click', () => {
+        removerCard(listItem)})
+
     const pokemonName = document.createElement('h2')
     pokemonName.classList.add('card--title')
     pokemonName.innerText = name
@@ -31,8 +39,37 @@ function createCard(name, image) {
     pokemonImage.setAttribute('src', image)
     listItem.append(pokemonImage)
 
+    
 
     cardList.append(listItem)
 }
 
+
+pokemonForm.addEventListener('submit', (element) => {
+    element.preventDefault()
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            name: nameInput.value,
+            image: urlInput.value,
+            liked: false
+        }),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            getCards()
+            cardList.innerHTML = ''
+        })
+})
+
+
 getCards()
+
+function removerCard(listItem) {
+    listItem.remove()
+    console.log('card removed')
+}
