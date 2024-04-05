69727f5eabd0b29243c88b8fe83139317c8660ab
Tim Zoltie <t.zoltie@gmail.com>
2024-04-05 14:58:22 +0100

    like_icon_added

--- index.js
+++ index.js
@@ -3,30 +3,41 @@ const pokemonForm = document.querySelector('#poke-form')
 const nameInput = document.querySelector('#name-input')
 const urlInput = document.querySelector('#image-input')
 const cardList = document.querySelector('.cards')
-
+const mainPage = document.querySelector('#home')
 
 function getCards() {
     fetch(url)
         .then(response => response.json())
         .then(json => {
             cardList.innerHTML = ''
-            json.forEach(pokemon => createCard(pokemon.name, pokemon.image));
+            json.forEach(pokemon => createCard(pokemon.name, pokemon.image, pokemon.liked));
         })
 }
 
 
-function createCard(name, image) {
+function createCard(name, image, liked) {
     const listItem = document.createElement('li')
     listItem.classList.add('card')
     
+    const cardHeader = document.createElement('section')
+    cardHeader.classList.add('card-header')
+    listItem.append(cardHeader)
     const deleteBtn = document.createElement('input')
     deleteBtn.classList.add('delete-button')
     deleteBtn.type = 'button'
     deleteBtn.value = 'X'
-    listItem.append(deleteBtn)
+    cardHeader.append(deleteBtn)
 
     deleteBtn.addEventListener('click', () => {
         removerCard(listItem)})
+
+
+    const likedBtn = document.createElement('img')
+    likedBtn.width = '30'
+    likedBtn.classList.add('like-button')
+    likedBtn.setAttribute('src', 'assets/svg/like.svg')
+    cardHeader.append(likedBtn)
+    
 
     const pokemonName = document.createElement('h2')
     pokemonName.classList.add('card--title')
@@ -39,10 +50,19 @@ function createCard(name, image) {
     pokemonImage.setAttribute('src', image)
     listItem.append(pokemonImage)
 
+    const updateBtn = document.createElement('input')
+    updateBtn.classList.add('update-button')
+    updateBtn.type = 'button'
+    updateBtn.value = 'Evolve'
+    listItem.append(updateBtn)
     
+    updateBtn.addEventListener('click', () => {
+        popUpMessage()
+    })
 
     cardList.append(listItem)
 }
+
 
 
 pokemonForm.addEventListener('submit', (element) => {
@@ -67,9 +87,65 @@ pokemonForm.addEventListener('submit', (element) => {
 })
 
 
+function updateCard() {
+    fetch(url, {
+        method: 'PUT', 
+        body: JSON.stringify({
+            name: nameInput.value,
+            image: urlInput.value,
+            liked: false
+        }),
+        headers: {
+            'Content-type': 'application/json'
+        }
+        .then((response) => response.json())
+        .then(json => {
+        getCards()
+        cardList.innerHTML = ''
+        })
+    })
+}
+
+
 getCards()
 
 function removerCard(listItem) {
     listItem.remove()
-    console.log('card removed')
+}
+
+
+function popUpMessage() {
+    const form = document.createElement('form')
+    form.setAttribute('id', 'update-form')
+
+    const nameLabel = document.createElement('label')
+    nameLabel.innerText = 'Name:'
+
+    const nameInput = document.createElement('input')
+    nameInput.setAttribute('id', 'updated-name-input')
+    nameInput.type = 'text'
+    nameInput.name = 'name'
+    nameInput.required
+    nameLabel.append(nameInput)
+    form.append(nameLabel)
+
+    const urlLabel = document.createElement('label')
+    urlLabel.innerText = 'Image URL:'
+
+    const urlInput = document.createElement('input')
+    urlInput.setAttribute('id', 'updated-image-input')
+    urlInput.type = 'text'
+    urlInput.name = 'image'
+    urlInput.required
+    urlLabel.append(urlInput)
+    form.append(urlLabel)
+
+    const submitInput = document.createElement('input')
+    submitInput.type = 'submit'
+    submitInput.value = 'EVOLVE POKEMON'
+    form.append(submitInput)
+
+    submitInput.addEventListener('submit', updateCard())
+
+    mainPage.append(form)
 }
