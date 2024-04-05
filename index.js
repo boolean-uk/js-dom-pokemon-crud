50af095e7d97937da0dfc593971d0703ee9c2d65
Will Baxter <bax.will@googlemail.com>
2024-04-05 12:30:35 +0100

    Added update section to pokemon card

--- index.js
+++ index.js
@@ -1,75 +1,110 @@
-const url = "https://boolean-api-server.fly.dev/MrStashy/pokemon/"
+const url = "https://boolean-api-server.fly.dev/MrStashy/pokemon/";
 
 //Build card
-const buildCard = pokemon => {
-    const li = document.createElement('li')
-    li.classList.add('card')
+const buildCard = (pokemon) => {
+  const li = document.createElement("li");
+  li.classList.add("card");
 
-    const h2 = document.createElement('h2')
-    h2.classList.add('card--title')
-    h2.innerText = pokemon.name
-    li.append(h2)
+  const h2 = document.createElement("h2");
+  h2.classList.add("card--title");
+  h2.innerText = pokemon.name;
+  li.append(h2);
 
-    const img = document.createElement('img')
-    img.setAttribute('width', 256)
-    img.setAttribute('src', pokemon.image)
-    img.classList.add('card--img')
-    li.append(img)
+  const img = document.createElement("img");
+  img.setAttribute("width", 256);
+  img.setAttribute("src", pokemon.image);
+  img.classList.add("card--img");
+  li.append(img);
 
-    const deleteButton = document.createElement('button')
-    deleteButton.setAttribute('id', 'delete-button')
-    deleteButton.classList.add('on-card-button')
-    deleteButton.innerText='Delete'
-    deleteButton.addEventListener('click', () => {
-        deletePokemon(pokemon)
-    })
-    li.append(deleteButton)
+  const updateDiv = document.createElement('div')
+  updateDiv.classList.add('update-section')
+  li.append(updateDiv)
 
-    const cardList = document.querySelector('.cards')
-    cardList.append(li)
-}
+  const h3 = document.createElement('h3')
+  h3.innerText = "Update Pokemon"
+  updateDiv.append(h3)
+
+  const newNameLabel = document.createElement("label");
+  newNameLabel.innerText = "Name: "
+  const newNameInput = document.createElement("input")
+  newNameInput.setAttribute('id', 'new-name-input')
+  newNameInput.setAttribute('type', 'text')
+  newNameLabel.append(newNameInput)
+  updateDiv.append(newNameLabel)
+
+  const newImageLabel = document.createElement("label");
+  newImageLabel.innerText = "Image: "
+  const newImageInput = document.createElement("input")
+  newImageInput.setAttribute('id', 'new-image-input')
+  newImageInput.setAttribute('type', 'text')
+  newImageLabel.append(newImageInput)
+  updateDiv.append(newImageLabel)
+
+  const updateButton = document.createElement('button')
+  updateButton.setAttribute('id', 'update-button')
+  updateButton.innerText = "Update"
+  updateButton.addEventListener('click', () => {
+    updatePokemon(pokemon)
+  })
+  updateDiv.append(updateButton)
+
+
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id", "delete-button");
+  deleteButton.classList.add("on-card-button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", () => {
+    deletePokemon(pokemon);
+  });
+  li.append(deleteButton);
+
+  const cardList = document.querySelector(".cards");
+  cardList.append(li);
+};
 
 const renderCards = async () => {
-    const cardList = document.querySelector('.cards')
+  const cardList = document.querySelector(".cards");
 
-    const response = await fetch(url)
-    const json = await response.json()
+  const response = await fetch(url);
+  const json = await response.json();
 
-    cardList.innerHTML = ''
-    json.forEach(buildCard)
-}
+  cardList.innerHTML = "";
+  json.forEach(buildCard);
+};
 
 //Add event listener to button
-const addForm = document.querySelector('#poke-form')
-addForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const nameInput = document.querySelector('#name-input')
-    const imgInput = document.querySelector('#image-input')
-    addNewPokemon(nameInput, imgInput)
-})
+const addForm = document.querySelector("#poke-form");
+addForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nameInput = document.querySelector("#name-input");
+  const imgInput = document.querySelector("#image-input");
+  addNewPokemon(nameInput, imgInput);
+});
 
 //Add pokemon
 const addNewPokemon = async (name, image) => {
-    await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({
-            name: name.value,
-            image: image.value,
-            liked: false
-        }),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
- renderCards()
-}
+  await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      name: name.value,
+      image: image.value,
+      liked: false,
+    }),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  name.value = "";
+  image.value = "";
+  renderCards();
+};
 
 //Delete pokemon
 const deletePokemon = async (pokemon) => {
-   await fetch(`${url}${pokemon.id}`, {
-    method: 'DELETE'
-   })
-   renderCards()
-}
+  await fetch(`${url}${pokemon.id}`, {
+    method: "DELETE",
+  });
+  renderCards();
+};
 
-renderCards()
+renderCards();
