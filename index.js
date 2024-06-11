c0e2013766479aa86a1838d7d0ffb6a2c65d0eb0
Thomas Jensen <thomas.jensen_@outlook.com>
2024-06-11 21:25:55 +0200

    Completed extensions

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const API_URL = 'https://boolean-api-server.fly.dev/th0jensen/pokemon'
 
+const state = {
+    isEditing: false,
+}
+
 function getPokemon() {
     fetch(API_URL)
         .then(async (response) => {
@@ -30,10 +34,49 @@ async function addPokemon(pokemon) {
     console.log(pokemonContent)
 }
 
+async function deletePokemon(pokemon) {
+    const postResponse = await fetch(API_URL + '/' + pokemon.id, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    })
+
+    const pokemonContent = await postResponse.json()
+    console.log(pokemonContent)
+}
+
+async function updatePokemon(pokemon) {
+    const postResponse = await fetch(API_URL + '/' + pokemon.id, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            id: pokemon.id,
+            name: pokemon.name,
+            image: pokemon.image,
+            liked: pokemon.liked,
+        }),
+    })
+
+    const pokemonContent = await postResponse.json()
+    console.log(pokemonContent)
+}
+
 // Render all the pokemon
 function generateList(pokemonim) {
     const cardsUL = document.querySelector('.cards')
     cardsUL.innerHTML = ''
+
+    if (state.isEditing) {
+        pokemonim.forEach((item) => {
+            cardsUL.appendChild(generateEditCard(item))
+        })
+        return
+    }
 
     pokemonim.forEach((item) => {
         cardsUL.appendChild(generateCard(item))
@@ -52,9 +95,82 @@ function generateCard(pokemon) {
 
     const pokemonImage = document.createElement('img')
     pokemonImage.width = 256
+    pokemonImage.height = 256
     pokemonImage.classList.add('card--img')
     pokemonImage.src = pokemon.image
     listItem.appendChild(pokemonImage)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Delete'
+    deleteButton.addEventListener('click', () =>
+        deletePokemon(pokemon).then(() => getPokemon())
+    )
+    listItem.appendChild(deleteButton)
+
+    const favouriteButton = document.createElement('button')
+    favouriteButton.innerText = pokemon.liked ? 'Remove favourite' : 'Favourite'
+    favouriteButton.addEventListener('click', () => {
+        if (pokemon.liked) {
+            pokemon.liked = false
+            updatePokemon(pokemon).then(() => getPokemon())
+        } else {
+            pokemon.liked = true
+            updatePokemon(pokemon).then(() => getPokemon())
+        }
+    })
+    listItem.appendChild(favouriteButton)
+
+    const editButton = document.createElement('button')
+    editButton.innerText = 'Edit'
+    editButton.addEventListener('click', () => {
+        state.isEditing = true
+        getPokemon()
+    })
+    listItem.appendChild(editButton)
+
+    return listItem
+}
+
+// Add edit functionality to the cards
+function generateEditCard(pokemon) {
+    const listItem = document.createElement('li')
+    listItem.classList.add('card')
+
+    const pokemonNameLabel = document.createElement('label')
+    pokemonNameLabel.innerText = 'Name'
+    listItem.appendChild(pokemonNameLabel)
+
+    const pokemonName = document.createElement('input')
+    pokemonName.value = pokemon.name
+    listItem.appendChild(pokemonName)
+
+    const breek = document.createElement('br')
+    listItem.appendChild(breek)
+
+    const pokemonImageLabel = document.createElement('label')
+    pokemonImageLabel.innerText = 'Image'
+    listItem.appendChild(pokemonImageLabel)
+
+    const pokemonImage = document.createElement('input')
+    pokemonImage.value = pokemon.image
+    listItem.appendChild(pokemonImage)
+
+    const breek2 = document.createElement('br')
+    listItem.appendChild(breek2)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Submit'
+    deleteButton.addEventListener('click', () => {
+        const updatedPokemon = {
+            id: pokemon.id,
+            name: pokemonName.value,
+            image: pokemonImage.value,
+            liked: pokemon.liked,
+        }
+        state.isEditing = false
+        updatePokemon(updatedPokemon).then(() => getPokemon())
+    })
+    listItem.appendChild(deleteButton)
 
     return listItem
 }
